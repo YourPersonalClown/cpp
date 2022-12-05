c1285586ca39df4dff25698d338e22c09d700f85
YourPersonalClown <daniel13hj@gmail.com>
2022-12-05 15:29:29 +0300

    add hw

---
+++ main8.cpp
@@ -0,0 +1,20 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int PPK;
+	float res;
+	cout << "price per kg" << endl;
+	cin >> PPK;
+
+	for (float x = 1.2; x < 2.1; x=x+0.1) {
+		res = PPK * x;
+		cout << "sweets price: " << res << endl;
+	};
+	return 0;
+}	
---
+++ main9.cpp
@@ -0,0 +1,22 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int n, k, i=0;
+	cout << "write n:";
+	cin >> n;	cout << endl;
+	cout << "write k:";
+	cin >> k;   cout << endl;
+	while (n>=k)
+	{
+		n -= k;
+		i++;
+	}
+	cout << "4astnoe "<< i << " ostatok " << n;
+	return 0;
+}	
