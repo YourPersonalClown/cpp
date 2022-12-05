8bf4be595453d4089bcce28754b0e7a024789397
YourPersonalClown <daniel13hj@gmail.com>
2022-12-05 15:24:19 +0300

    Create main6.cpp

---
+++ main6.cpp
@@ -0,0 +1,23 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int a[10],n;
+    cout << "write n:"; cin >> n;
+    cout << endl << "zapolnite masiv" <<endl ;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i]; cout << endl;
+    }
+    for (int i = 0; i < n; i += 2) {
+        cout << a[i] << endl;
+    }
+
+    for (int i = n - (n % 2) - 1; i >= 1; i -= 2) {
+        cout << a[i] << endl;
+    }
+}	
