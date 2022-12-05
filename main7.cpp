f98eca0861b6540533c8f15715237712eda060c4
YourPersonalClown <daniel13hj@gmail.com>
2022-12-05 15:25:01 +0300

    Create main7.cpp

---
+++ main7.cpp
@@ -0,0 +1,27 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int a[10][10], n, m, k, sum=0, mult=1;
+    cout << "write m:"; cin >> m;
+    cout << "write n:"; cin >> n;
+    cout << "write k:"; cin >> k;
+    cout << endl << "zapolnite masiv" <<endl ;
+    for (int i = 0; i < m; ++i) {
+        for (int r = 0; r < n; ++r) {
+            cin >> a[i][r]; cout << endl;
+        }
+    }
+    for (int i = 0; i < m; ++i) {
+            sum = sum+a[i][k];
+            mult *= a[i][k];
+        }
+    cout <<  "sum:" << sum << "umnojenie:" << mult << endl;
+    
+
+}	
